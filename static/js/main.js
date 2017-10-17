b0cbcaab9b17f3404df3d38c293d6804fc5ad55e
Johan Sylvan <gtc124@gmail.com>
2017-10-17 14:52:08 +0200

    Too much JS.

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,41 @@
-// Variables for all things needed(what can I say?)
-var navbar = document.getElementById("navbar");
-var toggle_nav = document.getElementById('toggle-nav');
-var first_section = document.getElementById('first-section');
-var link_list = document.getElementById('link-list');
+window.addEventListener('DOMContentLoaded', function(){
+  // Variables for all things needed(what can I say?)
+  var navbar = document.getElementById("navbar");
+  var toggle_nav = document.getElementById('toggle-nav');
+  var first_section = document.getElementById('first-section');
+  var link_list = document.getElementById('link-list');
 
-// Toggles "up"-class on navbar to... bring it up.
-function toggle()
-{
-      navbar.classList.toggle("up");
-      event.preventDefault();
-}
+  // Toggles "up"-class on navbar to... bring it up.
+  function toggle()
+  {
+    navbar.classList.toggle("up");
+    event.preventDefault();
+  }
 
-// Listen for scroll-event and change classes accordingly
-window.addEventListener('scroll', function(e)
-{
-      toggle_nav.innerHTML = window.pageYOffset;
-      if(window.pageYOffset > 0){
-            if(!navbar.classList.contains('fix'))
-            {
-                  navbar.classList.add("fix");
-                  first_section.classList.add('top');
-            }
-      }else if (window.pageYOffset == 0){
-            navbar.classList.remove("fix");
-            first_section.classList.remove('top');
+  Array.prototype.slice.call(document.getElementsByTagName('section')).forEach(function(value, index){
+    value.innerHTML = value.getBoundingClientRect().top;
+  });
+
+  // Listen for scroll-event and change classes accordingly
+  window.addEventListener('scroll', function(e)
+  {
+    toggle_nav.innerHTML = window.pageYOffset;
+    if(window.pageYOffset > 0){
+      if(!navbar.classList.contains('fix'))
+      {
+        navbar.classList.add("fix");
+        first_section.classList.add('top');
       }
-}, true)
+    }else if (window.pageYOffset == 0){
+      navbar.classList.remove("fix");
+      first_section.classList.remove('top');
+    }
+  }, true)
 
-navbar.addEventListener('click', function(e)
-{
-  //console.log(window.innerWidth);
-  if(navbar != e.target || window.innerWidth > 768) return;
-  link_list.classList.toggle('selected');
+  navbar.addEventListener('click', function(e)
+  {
+    //console.log(window.innerWidth);
+    if(navbar != e.target || window.innerWidth > 768) return;
+    link_list.classList.toggle('selected');
+  })
 })
