fd09cfe97a6b6292882aeb70388a754196f80967
Johan Sylvan <gtc124@gmail.com>
2017-11-08 21:12:36 +0100

    Made current-page-thingy-thing almost work, but not completely

--- static/js/main.js
+++ static/js/main.js
@@ -12,8 +12,28 @@ for(let i = 0; i < pages.length; i++){
     let index = document.createTextNode(i+1);
     li.appendChild(index);
     page_list.appendChild(li);
-    if(i !== (pages.length-1)){
-        let span = document.createElement('span');
-        page_list.appendChild(span);
+    // if(i !== (pages.length-1)){
+    //     let span = document.createElement('span');
+    //     page_list.appendChild(span);
+    // }
+}
+
+console.log(window.innerHeight);
+
+for(let i = 0; i < pages.length; i++){
+    if(window.pageYOffset >= pages[i].offsetTop){
+        page_list.children[i].classList.add('active');
+        break;
     }
 }
+
+window.addEventListener('scroll', function(event){
+    for(let i = 0; i < pages.length; i++){
+        if(window.pageYOffset >= pages[i].offsetTop &&
+            !page_list.children[i].classList.contains('active')){
+            document.getElementsByClassName('active')[0].classList.remove('active');
+            page_list.children[i].classList.add('active');
+            break;
+        }
+    }
+});
