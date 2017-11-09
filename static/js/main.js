202901158863e2e464abc725058b3aa9bec5d46b
Johan Sylvan <gtc124@gmail.com>
2017-11-09 09:36:51 +0100

    Current-page is now 75% working, addad a couple more articles to show.

--- static/js/main.js
+++ static/js/main.js
@@ -4,35 +4,48 @@ function infoToggle(event,elmnt)
     elmnt.parentNode.parentNode.classList.toggle('expanded');
 }
 
-const pages = document.getElementById("info-section").children;
-const page_list = document.getElementById("page-list");
+const pages = document.querySelector('#info-section').children;
+const page_list = document.querySelector('#page-list');
 
-for(let i = 0; i < pages.length; i++){
+let scrolling = false;
+
+for(let i = 0; i < pages.length; i++)
+{
     let li = document.createElement('li');
     let index = document.createTextNode(i+1);
     li.appendChild(index);
     page_list.appendChild(li);
-    // if(i !== (pages.length-1)){
-    //     let span = document.createElement('span');
-    //     page_list.appendChild(span);
-    // }
-}
-
-console.log(window.innerHeight);
-
-for(let i = 0; i < pages.length; i++){
-    if(window.pageYOffset >= pages[i].offsetTop){
-        page_list.children[i].classList.add('active');
-        break;
+    if(i !== (pages.length-1)){
+        let span = document.createElement('span');
+        page_list.appendChild(span);
     }
 }
 
-window.addEventListener('scroll', function(event){
-    for(let i = 0; i < pages.length; i++){
-        if(window.pageYOffset >= pages[i].offsetTop &&
-            !page_list.children[i].classList.contains('active')){
-            document.getElementsByClassName('active')[0].classList.remove('active');
-            page_list.children[i].classList.add('active');
+for(let i = 0; i < pages.length; i++)
+{
+    if((window.pageYOffset + (window.innerHeight/2)) >= (pages[i].offsetTop + (pages[i].offsetHeight/2)))
+    {
+        page_list.children[i].classList.add('active');
+        break;
+    }
+    page_list.children[i].addEventListener('click', function(event)
+    {
+        console.log(event);
+    });
+}
+
+console.log(document.querySelector('#page-list li::before'));
+
+window.addEventListener('scroll', function(event)
+{
+    for(let i = 0; i < pages.length; i++)
+    {
+        if(window.pageYOffset >= pages[i].offsetTop/2 &&
+            window.pageYOffset <= pages[i].offsetTop &&
+            !page_list.children[i].classList.contains('active'))
+        {
+            document.querySelector('li.active').classList.remove('active');
+            page_list.querySelectorAll('li')[i].classList.add('active');
             break;
         }
     }
