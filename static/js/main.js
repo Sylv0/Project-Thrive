3d81b56a45e6b3c76590fcc8cd850379ef0711db
Johan Sylvan <gtc124@gmail.com>
2017-11-09 11:26:31 +0100

    Put most js to run when document is loaded

--- static/js/main.js
+++ static/js/main.js
@@ -4,43 +4,45 @@ const infoToggle = (event,elmnt) =>
     elmnt.parentNode.parentNode.classList.toggle('expanded');
 }
 
-const pages = document.querySelector('#info-section').children;
-const page_list = document.querySelector('#page-list');
+window.onload = () => {
+    const pages = document.querySelector('#info-section').children;
+    const page_list = document.querySelector('#page-list');
 
-let scrolling = false;
+    let scrolling = false;
 
-for(let i = 0; i < pages.length; i++)
-{
-    let li = document.createElement('li');
-    let index = document.createTextNode(i+1);
-    li.appendChild(index);
-    page_list.appendChild(li);
-    if(i !== (pages.length-1)){
-        let span = document.createElement('span');
-        page_list.appendChild(span);
-    }
-}
-
-for(let i = 0; i < pages.length; i++)
-{
-    if((window.pageYOffset + (window.innerHeight/2)) >= (pages[i].offsetTop + (pages[i].offsetHeight/2)))
-    {
-        page_list.children[i].classList.add('active');
-        break;
-    }
-}
-
-window.addEventListener('scroll', (event) =>
-{
     for(let i = 0; i < pages.length; i++)
     {
-        if(window.pageYOffset >= pages[i].offsetTop/2 &&
-            window.pageYOffset <= pages[i].offsetTop &&
-            !page_list.children[i].classList.contains('active'))
+        let li = document.createElement('li');
+        let index = document.createTextNode(i+1);
+        li.appendChild(index);
+        page_list.appendChild(li);
+        if(i !== (pages.length-1)){
+            let span = document.createElement('span');
+            page_list.appendChild(span);
+        }
+    }
+
+    for(let i = 0; i < pages.length; i++)
+    {
+        if((window.pageYOffset + (window.innerHeight/2)) >= (pages[i].offsetTop + (pages[i].offsetHeight/2)))
         {
-            document.querySelector('li.active').classList.remove('active');
-            page_list.querySelectorAll('li')[i].classList.add('active');
+            page_list.children[i].classList.add('active');
             break;
         }
     }
-});
+
+    window.addEventListener('scroll', (event) =>
+    {
+        for(let i = 0; i < pages.length; i++)
+        {
+            if(window.pageYOffset >= pages[i].offsetTop/2 &&
+                window.pageYOffset <= pages[i].offsetTop &&
+                !page_list.children[i].classList.contains('active'))
+            {
+                document.querySelector('li.active').classList.remove('active');
+                page_list.querySelectorAll('li')[i].classList.add('active');
+                break;
+            }
+        }
+    });
+}
